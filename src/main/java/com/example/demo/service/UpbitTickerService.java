992e90efd0f5b2db41c5ae711f5b5f12f19b74d0
swyoo5 <3040sw@naver.com>
2024-12-04 16:33:48 +0900

    Exception 오류 수정

--- src/main/java/com/example/demo/service/UpbitTickerService.java
+++ src/main/java/com/example/demo/service/UpbitTickerService.java
@@ -1,7 +1,8 @@
 package com.example.demo.service;
 
+import java.io.IOException;
 import java.util.Map;
 
 public interface UpbitTickerService {
-    public Map<String, Object>[] getTicker(String market);
+    public Map<String, Object>[] getTicker(String market) throws IOException;
 }
